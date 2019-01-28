3e2f610eab2f88fcc9c3a6c3cbe434da4a5f5587
Vladyslav <bukow.wlad@yandex.ua>
2019-01-28 12:43:35 +0200

    added custom/broadcasting events

--- public/main.js
+++ public/main.js
@@ -2,7 +2,16 @@ let socket = io();
 
 socket.on('connect', ()=> {
 	console.log('Connected to server');
+
+	// socket.emit('createMessage', {
+	// 	from: 'Waldermar',
+	// 	text: 'Wats going on!'
+	// });
 });
 socket.on('disconnect', ()=> {
 	console.log('disconnected from server');
+});
+
+socket.on('newMessage', (message) => {
+	console.log('New Message!', message);
 });
--- server/server.js
+++ server/server.js
@@ -12,6 +12,32 @@ let io = socketIO(server);
 io.on('connection', (socket)=>{
 	console.log('A new user just connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app!',
+		createdAt: new Date().getTime()
+	}); 
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined!',
+		createdAt: new Date().getTime()
+	}); 
+
+	socket.on('createMessage' , (message) => {
+		console.log('Create message', message);			
+		io.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+			createAt : new Date().getTime() 
+		});
+		// socket.broadcast.emit('newMessage',{
+		// 		from: message.from,
+		// 		text: message.text,
+		// 		createAt : new Date().getTime() 
+		// 	})
+	});
+
 	socket.on('disconnect', ()=> {
 		console.log('User was disconnected');
 	});
